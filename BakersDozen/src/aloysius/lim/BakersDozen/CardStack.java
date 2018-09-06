1f8ebadcc8c266dbfe26ee22afcb5a12599bfc9a
all1913 <all1913@R2055PC9.ad.utulsa.edu>
2018-09-06 16:38:49 -0500

    update lesson progress

---
+++ BakersDozen/src/aloysius/lim/BakersDozen/CardStack.java
@@ -0,0 +1,72 @@
+package aloysius.lim.BakersDozen;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+public class CardStack {
+/**
+ * Author: Aloysius Lim
+ * Date Created: 9-6-18
+ * Last Updated: 9-6-18
+ */
+	
+	/**** Variables ****/
+	
+	ArrayList<Card> cards = new ArrayList<>();
+	int stackX = 0;
+	int stackY = 0;
+	int overlap = 0;
+	
+	//Constructor
+	public CardStack(int stackX, int stackY, int overlap) {
+		this.stackX = stackX;
+		this.stackY = stackY;
+		this.overlap = overlap;
+	}
+	
+	public void add(Card card) {
+		int cardX = stackX;
+		int cardY = stackY + overlap * cards.size();
+		card.setXY(cardX, cardY);
+		cards.add(card);
+	}
+	
+	public void addToBeginning(Card card) {
+		card.setXY(stackX, stackY);
+		cards.add(0,card);
+		
+		for (int i=1; i<cards.size(); i++) {
+			Card nextCard = cards.get(i);
+			nextCard.addToXY(0, overlap);
+		}
+	}
+	
+	public void draw(Graphics g) {
+		if (cards.size() > 0 && overlap == 0) {
+			int lastIndex = cards.size()-1;
+			Card card = cards.get(lastIndex);
+			card.draw(g);
+		}else {
+			for (int i=0; i<cards.size(); i++) {
+				Card card = cards.get(i);
+				card.draw(g);
+			}
+		}
+	}
+	
+	public int size() {
+		return cards.size();
+	}
+	
+	public int getX() {
+		return stackX;
+	}
+	
+	public int getY() {
+		return stackY;
+	}
+	
+	public void clear() {
+		cards.clear();
+	}
+}
--- BakersDozen/src/aloysius/lim/BakersDozen/TablePanel.java
+++ BakersDozen/src/aloysius/lim/BakersDozen/TablePanel.java
@@ -1,5 +1,6 @@
 package aloysius.lim.BakersDozen;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
@@ -26,11 +27,35 @@ public class TablePanel extends JPanel{
 	private static final int WIDTH = CARDWIDTH * 13 + SPACING * 12 + MARGIN * 2; //Width of table
 	private static final int HEIGHT = CARDHEIGHT * 9 + MARGIN * 3; //Height of table
 	
+	private static final int FOUNDATIONX = WIDTH/2 - (4*CARDWIDTH + 3*SPACING)/2;
+	private static final int FOUNDATIONY = MARGIN;
+	private static final int BOARDX = MARGIN;
+	private static final int BOARDY = CARDHEIGHT + (MARGIN*2);
+	private static final int OVERLAP = (int) (CARDHEIGHT * 0.65);
+	
 	private Deck deck;
 	private Card card;
+	private CardStack[] foundation = new CardStack[4];
+	private CardStack[] column = new CardStack[13];
 
 	//Constructor
 	public TablePanel() {
+		int x = FOUNDATIONX;
+		int y = FOUNDATIONY;
+		
+		for (int i = 0;i<foundation.length;i++) {
+			foundation[i] = new CardStack(x,y,0);
+			x += CARDWIDTH + SPACING;
+		}
+		
+		x = BOARDX;
+		y = BOARDY;
+		
+		for (int i = 0;i<column.length;i++) {
+			column[i] = new CardStack(x,y,OVERLAP);
+			x += CARDWIDTH + SPACING;
+		}
+		
 		deck = new Deck();
 		deal();
 	}
@@ -41,12 +66,34 @@ public class TablePanel extends JPanel{
 	}
 
 	private void deal() {
-		card = deck.deal();
-		card.setXY(10, 10);
+//		card = deck.deal();
+//		card.setXY(10, 10);
+		for(int row = 0; row<4; row++){
+			for (int col=0; col< 13; col++) {
+				Card card = deck.deal();
+				column[col].add(card);
+			}
+		}
+		repaint();
 	}
 	
 	public void paintComponent(Graphics g) {
-		card.draw(g);
+//		card.draw(g);
+		
+		//draw background
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		//foundation cards
+		for (int i=0;i<4;i++) {
+			if (foundation[i].size() > 0) {
+				foundation[i].draw(g);
+			}else {
+				int x = foundation[i].getX();
+				int y = foundation[i].getY();
+				Card.drawOutline(g, x, y);
+			}
+		}
 	}
 	
 }
